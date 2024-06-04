7fbb03a5997ef8ed865de620929a8890d02e4291
FuzzyYeti <j.son@digipen.edu>
2024-06-04 10:42:50 +0900

    Maybe Finish Scout Class
    (I hope)

--- CS170/polymorphic_player/source/Scout.cpp
+++ CS170/polymorphic_player/source/Scout.cpp
@@ -1,2 +1,44 @@
 #include "Scout.h"
+namespace HLP2
+{
+    Scout::Scout(const std::string &name, int health, int primary_damage, int secondary_damage)
+        : IPlayer(name, health, primary_damage, secondary_damage){};
 
+    
+    std::string Scout::WhoAmI() const
+    {
+        return "I'm a Scout named ";
+    }
+
+    const char *Scout::GetType() const
+    {
+        return "SCOUT";
+    }
+
+    void Scout::PrimaryAttack(IPlayer &other) const
+    {
+        int damage_amount = this->GetPrimaryDamage();
+        other.TakeDamage(damage_amount);
+        std::cout<<"pushed"<<'\n';
+    }
+    void Scout::SecondaryAttack(IPlayer &other) const
+    {
+        int damage_amount = this->GetSecondaryDamage();
+        other.TakeDamage(damage_amount);
+        std::cout<<"stabbed"<<'\n';
+    }
+
+    void Scout::TakeDamage(int damage)
+    {
+        // I feel so proud of this code
+        IPlayer::setHealth(IPlayer::GetHealth() - damage);
+    }
+
+    IPlayer *Scout::Clone() const
+    {
+        //we have to do this return local variable is em not good situation i think       
+        IPlayer* temp= new Scout(*this);
+        return temp;
+    }
+
+}
--- CS170/polymorphic_player/source/Scout.h
+++ CS170/polymorphic_player/source/Scout.h
@@ -1,6 +1,28 @@
+#include"Player.h"
+#include<iostream>
 #pragma once
 
 namespace HLP2
 {
-
+  class Scout : public IPlayer
+    {
+    public:
+        Scout();
+        explicit Scout(const std::string& name, int health = 50, int primary_damage = 1,int secondary_damage = 5);
+        explicit Scout(const Scout&rhs);
+        std::string WhoAmI() const override;
+        const char* GetType() const override;
+        
+        //Gave some damage to other player paramter is attack target 
+        void PrimaryAttack(IPlayer& other) const override;
+        void SecondaryAttack(IPlayer& other) const override;
+        // Constructor declaration
+        // Overriding virtual functions declarations
+        void TakeDamage(int damage) override;
+        //Declare the Clone function that returns a pointer to an IPlayer object.
+        // This function should create a new Scout object by using the copy constructor and return a pointer to the newly created object: 
+        IPlayer*            Clone() const override;
+    private:
+        // Implement private helper functions if necessary
+    };
 }
